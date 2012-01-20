3e2a28dfa99123378e8a1471ef657d87cf6e0cd6
Kenytt Avery <kavery@fullerton.edu>
2012-01-19 19:33:03 -0800

    Added user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,9 +5,19 @@ class Micropost < ActiveRecord::Base
 
   default_scope :order => 'microposts.created_at DESC'
 
+  scope :from_users_followed_by, lambda { |user| followed_by user }
+
   validates :content,
     :presence => true,
     :length => { :maximum => 140 }
   validates :user_id,
     :presence => true
+
+  private
+
+    def self.followed_by(user)
+      following_ids = %(SELECT followed_id FROM relationships WHERE follower_id = :user_id)
+      where("user_id IN (#{following_ids}) OR user_id = :user_id", { :user_id => user })
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,23 @@ class User < ActiveRecord::Base
 
   has_many :microposts, :dependent => :destroy
 
+  has_many :relationships,
+    :foreign_key => 'follower_id',
+    :dependent => :destroy
+
+  has_many :reverse_relationships,
+    :foreign_key => 'followed_id',
+    :class_name => 'Relationship',
+    :dependent => :destroy
+
+  has_many :followers,
+    :through => :reverse_relationships,
+    :source => :follower
+
+  has_many :following,
+    :through => :relationships,
+    :source => :followed
+
   validates :name,
     :presence => true,
     :length   => { :maximum => 50 }
@@ -23,7 +40,7 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
 
   def feed
-    Micropost.where 'User_id = ?', id
+    Micropost.from_users_followed_by self
   end
 
   def has_password?(submitted_password)
@@ -39,6 +56,18 @@ class User < ActiveRecord::Base
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id id
     (user && user.salt == cookie_salt) ? user : nil
+  end
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create! :followed_id => followed.id
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
 
   private
